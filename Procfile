af7c7283e19988a2eb50ebf766583ebd599840fc
Sirvan Safabakhsh <sirvan.safabakhsh@yahoo.com>
2025-07-26 15:19:27 +0330

    dd

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: python -m proxy --hostname 0.0.0.0 --port $PORT --basic-auth $BASIC_AUTH
--- start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python -m proxy --hostname 0.0.0.0 --port $PORT --basic-auth lans_sa1328:Sklmndufz/2003
